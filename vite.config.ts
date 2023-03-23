5e49ffdbf4cf4ff7357913672bbafb57f64c902e
bangkitwira <bangkitwira@gmail.com>
2023-03-23 16:35:54 +0700

    update base url for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://bangkitwira16.github.io/GitHub-repositories-explorer',
   define: {
     'process.env': {}
   },
